f2649bc81b40126e0d0d67df14b1e914bc0e0910
KyleRoger <kyleroger@hotmail.com>
2018-11-25 20:32:30 -0500

    Initial Commit, Project Set-Up.

---
+++ SQ1-A2-TDD/TriangleUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5628a3c-9583-4e19-b6dd-dffb33cd6f04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SQ1-A2-TDD/TriangleUnitTests/TriangleUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQ1_A2_TDD;
+
+namespace TriangleUnitTests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        Triangle testTriangle = new Triangle();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
