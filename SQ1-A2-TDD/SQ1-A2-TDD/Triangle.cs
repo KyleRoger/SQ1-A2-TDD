7394809836341a1b7d49257cd1eda9470546a745
KyleRoger <kyleroger@hotmail.com>
2018-11-25 20:37:44 -0500

    First Unit Test - Normal Hypoteneuse Test - PASS

--- SQ1-A2-TDD/SQ1-A2-TDD/Triangle.cs
+++ SQ1-A2-TDD/SQ1-A2-TDD/Triangle.cs
@@ -20,7 +20,8 @@ namespace SQ1_A2_TDD
         public double CalculateHyp(double A, double B)
         {
             double C = 0;
-
+            // (A^2 + B^2) = C^2
+            C = Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
             return C;
         }
     }
